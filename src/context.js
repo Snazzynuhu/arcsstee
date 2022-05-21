412ecd94568a72dca839f78502c9e50cae7efe83
HARUNA NUHU <harunanuhu17@gmail.com>
2022-05-21 17:11:59 +0100

    updated some profile images

--- src/context.js
+++ src/context.js
@@ -1,21 +1,22 @@
-import React,{useState,useCallback,useContext,useEffect} from "react";
-const url = 'http://link to json data of graduates';
-const AppContext = React.createContext();
-const AppProvider = ({children}) =>{
-    const [loading, setLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('a');
-    const [graduates, setGraduates] = useState([]);
-    return(
-        <AppContext.Provider value={{
-            loading,
-            setSearchTerm,
-            graduates,
-        }}>
-            {children}
-        </AppContext.Provider>
-    )
-}
-export const useGlobalContext = () =>{
-    return useContext(AppContext)
-}
-export {AppContext, AppProvider}
+// import React, { useState, useContext, useEffect } from "react";
+// // const url = 'http://link to json data of graduates';
+// const AppContext = React.createContext();
+// const AppProvider = ({ children }) => {
+//     const [loading, setLoading] = useState(false);
+//     const [searchTerm, setSearchTerm] = useState('a');
+//     const [graduates, setGraduates] = useState([]);
+//     return ( <
+//         AppContext.Provider value = {
+//             {
+//                 loading,
+//                 setSearchTerm,
+//                 graduates,
+//             }
+//         } > { children } <
+//         /AppContext.Provider>
+//     )
+// }
+// export const useGlobalContext = () => {
+//     return useContext(AppContext)
+// }
+// export { AppContext, AppProvider }
